c8457659f2cc47284ad9d4b092726e3ff44d28ca
Matt Makai <matthew.makai@gmail.com>
2016-11-23 14:06:52 -0800

    fix typos

--- no-framework/phone-calls/phone_calls.py
+++ no-framework/phone-calls/phone_calls.py
@@ -20,9 +20,11 @@ client = TwilioRestClient("ACxxxxxxxxxx", "yyyyyyyyyy")
 def dial_numbers(numbers_list):
     """Dials one or more phone numbers from a Twilio phone number."""
     for number in numbers_list:
-        print("Dialing {0}" % number)
+        print("Dialing " + number)
+        # set the method to "GET" from default POST because Amazon S3 only
+        # serves GET requests on files. Typically POST would be used for apps
         client.calls.create(to=number, from_=TWILIO_PHONE_NUMBER,
-                            url=TWIML_INSTRCUTIONS_URL)
+                            url=TWIML_INSTRUCTIONS_URL, method="GET")
 
 
 if __name__ == "__main__":
