2d81550a826d996870a7cc34af2db3f89f4ac786
Matt Makai <matthew.makai@gmail.com>
2019-10-25 12:04:29 -0400

    add django sms example

---
+++ django/djsms/broadcast/views.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.http import HttpResponse
+from twilio.rest import Client
+
+
+def broadcast_sms(request):
+    message_to_broadcast = ("Have you played the incredible TwilioQuest "
+                           "yet? Grab it here: https://www.twilio.com/quest")
+    client = Client()
+    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+        if recipient:
+            client.messages.create(to=recipient,
+                                   from_=settings.TWILIO_NUMBER,
+                                   body=message_to_broadcast)
+    return HttpResponse("messages sent!", 200)
