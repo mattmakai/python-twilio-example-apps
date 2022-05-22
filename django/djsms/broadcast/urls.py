70ec40a9d6e7078fbfa049be040b2f654895b08c
Kailash Choudhary <kailashjat604@gmail.com>
2022-05-22 10:18:14 +0530

    Update urls.py file

--- django/djsms/broadcast/urls.py
+++ django/djsms/broadcast/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'broadcast$', views.broadcast_sms, name="default"),
+    url('broadcast/', views.broadcast_sms, name="default"),
 ]
 
